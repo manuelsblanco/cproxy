3321beb565063192ce7f656239cfcd9dc7575a88
Manuel Blanco <mannywhite89@icloud.com>
2025-11-07 21:55:36 +0100

    Agregar archivo de configuración cproxy.conf y actualizar la estructura de configuración en config.h

--- include/config.h
+++ include/config.h
@@ -1,6 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <netinet/in.h>
+
+struct config {
+    in_port_t listen_port;
+    char backend_host[256];
+    in_port_t backend_port;
+}
+
 int load_config(const char *filename);
+const struct config* get_config();
 
 #endif
